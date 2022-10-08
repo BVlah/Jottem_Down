27736b881423f753d900f04eb2fc2a160027e1e9
Vlah <brennan.vlahcevic@armssoftware.com>
2022-10-08 16:04:35 -0400

    added post request

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,8 +6,18 @@ app.get('/notes', (req, res) => {
     res.json(results);
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/notes', (req, res) => {
+    data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+    newNote = req.body;
+    for (let i=0; i< data.length; i++) {
+        newNote.id = i+2
+    };
 
+    data.push(newNote);
+
+    fs.writeFileSync('./db/db.json', JSON.stringify(data));
+
+    res.json(data);
 });
 
 app.delete('/api/notes:id', (req, res) => {
